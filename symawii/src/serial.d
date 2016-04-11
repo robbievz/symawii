080741a222fbeae419be7cdbcbcd6ce3a3d2a945
Gerrie van Zyl <gerrie@van-zyl.net>
2016-04-11 15:48:53 -0400

    Not sure what I am doing

--- symawii/src/serial.d
+++ symawii/src/serial.d
@@ -1,20 +1,21 @@
 src/serial.o: src/serial.c lib-Mini51/hal/lib_serial.h \
- lib-Mini51/hal/lib_timers.h lib-Mini51/hal/lib_fp.h lib-Mini51/hal/hal.h \
- lib-Mini51/Device/Nuvoton/Mini51Series/Include/Mini51Series.h \
- lib-Mini51/CMSIS/Include/core_cm0.h \
- lib-Mini51/CMSIS/Include/core_cmInstr.h \
- lib-Mini51/CMSIS/Include/core_cmFunc.h \
- lib-Mini51/Device/Nuvoton/Mini51Series/Include/system_Mini51Series.h \
- lib-Mini51/StdDriver/inc/sys.h lib-Mini51/StdDriver/inc/clk.h \
- lib-Mini51/StdDriver/inc/adc.h lib-Mini51/StdDriver/inc/fmc.h \
- lib-Mini51/StdDriver/inc/gpio.h lib-Mini51/StdDriver/inc/i2c.h \
- lib-Mini51/StdDriver/inc/pwm.h lib-Mini51/StdDriver/inc/spi.h \
- lib-Mini51/StdDriver/inc/timer.h lib-Mini51/StdDriver/inc/uart.h \
- lib-Mini51/StdDriver/inc/wdt.h src/symawii.h src/rx.h src/defs.h \
- src/config.h src/config_X4.h src/options.h \
- lib-Mini51/hal/lib_digitalio.h src/projectsettings.h src/output.h \
- src/checkboxes.h src/vectors.h src/serial.h src/compass.h src/eeprom.h \
- src/imu.h src/gps.h
+  lib-Mini51/hal/lib_timers.h lib-Mini51/hal/lib_fp.h \
+  lib-Mini51/hal/hal.h \
+  lib-Mini51/Device/Nuvoton/Mini51Series/Include/Mini51Series.h \
+  lib-Mini51/CMSIS/Include/core_cm0.h \
+  lib-Mini51/CMSIS/Include/core_cmInstr.h \
+  lib-Mini51/CMSIS/Include/core_cmFunc.h \
+  lib-Mini51/Device/Nuvoton/Mini51Series/Include/system_Mini51Series.h \
+  lib-Mini51/StdDriver/inc/sys.h lib-Mini51/StdDriver/inc/clk.h \
+  lib-Mini51/StdDriver/inc/adc.h lib-Mini51/StdDriver/inc/fmc.h \
+  lib-Mini51/StdDriver/inc/gpio.h lib-Mini51/StdDriver/inc/i2c.h \
+  lib-Mini51/StdDriver/inc/pwm.h lib-Mini51/StdDriver/inc/spi.h \
+  lib-Mini51/StdDriver/inc/timer.h lib-Mini51/StdDriver/inc/uart.h \
+  lib-Mini51/StdDriver/inc/wdt.h src/symawii.h src/rx.h src/defs.h \
+  src/config.h src/config_X4.h src/options.h \
+  lib-Mini51/hal/lib_digitalio.h src/projectsettings.h src/output.h \
+  src/checkboxes.h src/vectors.h src/serial.h src/compass.h src/eeprom.h \
+  src/imu.h src/gps.h
 
 lib-Mini51/hal/lib_serial.h:
 
