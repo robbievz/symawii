5af1c651674eb526107f930a56581591c8e2d899
Gerrie van Zyl <gerrie@van-zyl.net>
2016-04-11 16:34:37 -0400

    Compiling under Xcode

---
+++ symawii/lib-Mini51/hal/lib_soft_3_wire_spi.d
@@ -0,0 +1,68 @@
+lib-Mini51/hal/lib_soft_3_wire_spi.o: \
+ lib-Mini51/hal/lib_soft_3_wire_spi.c lib-Mini51/hal/hal.h \
+ lib-Mini51/Device/Nuvoton/Mini51Series/Include/Mini51Series.h \
+ lib-Mini51/CMSIS/Include/core_cm0.h \
+ lib-Mini51/CMSIS/Include/core_cmInstr.h \
+ lib-Mini51/CMSIS/Include/core_cmFunc.h \
+ lib-Mini51/Device/Nuvoton/Mini51Series/Include/system_Mini51Series.h \
+ lib-Mini51/StdDriver/inc/sys.h lib-Mini51/StdDriver/inc/clk.h \
+ lib-Mini51/StdDriver/inc/adc.h lib-Mini51/StdDriver/inc/fmc.h \
+ lib-Mini51/StdDriver/inc/gpio.h lib-Mini51/StdDriver/inc/i2c.h \
+ lib-Mini51/StdDriver/inc/pwm.h lib-Mini51/StdDriver/inc/spi.h \
+ lib-Mini51/StdDriver/inc/timer.h lib-Mini51/StdDriver/inc/uart.h \
+ lib-Mini51/StdDriver/inc/wdt.h lib-Mini51/hal/lib_soft_3_wire_spi.h \
+ lib-Mini51/hal/lib_digitalio.h src/projectsettings.h src/defs.h \
+ src/config.h src/config_X4.h src/options.h src/output.h \
+ lib-Mini51/hal/lib_fp.h
+
+lib-Mini51/hal/hal.h:
+
+lib-Mini51/Device/Nuvoton/Mini51Series/Include/Mini51Series.h:
+
+lib-Mini51/CMSIS/Include/core_cm0.h:
+
+lib-Mini51/CMSIS/Include/core_cmInstr.h:
+
+lib-Mini51/CMSIS/Include/core_cmFunc.h:
+
+lib-Mini51/Device/Nuvoton/Mini51Series/Include/system_Mini51Series.h:
+
+lib-Mini51/StdDriver/inc/sys.h:
+
+lib-Mini51/StdDriver/inc/clk.h:
+
+lib-Mini51/StdDriver/inc/adc.h:
+
+lib-Mini51/StdDriver/inc/fmc.h:
+
+lib-Mini51/StdDriver/inc/gpio.h:
+
+lib-Mini51/StdDriver/inc/i2c.h:
+
+lib-Mini51/StdDriver/inc/pwm.h:
+
+lib-Mini51/StdDriver/inc/spi.h:
+
+lib-Mini51/StdDriver/inc/timer.h:
+
+lib-Mini51/StdDriver/inc/uart.h:
+
+lib-Mini51/StdDriver/inc/wdt.h:
+
+lib-Mini51/hal/lib_soft_3_wire_spi.h:
+
+lib-Mini51/hal/lib_digitalio.h:
+
+src/projectsettings.h:
+
+src/defs.h:
+
+src/config.h:
+
+src/config_X4.h:
+
+src/options.h:
+
+src/output.h:
+
+lib-Mini51/hal/lib_fp.h:
---
+++ symawii/lib-Mini51/startup_Mini51Series.d
@@ -0,0 +1,1 @@
+lib-Mini51/startup_Mini51Series.o: lib-Mini51/startup_Mini51Series.c
